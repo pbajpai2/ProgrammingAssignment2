e584a47fc091c60acfadac4e884692a723e36a7c
Pushkar Bajpai <37352019+pbajpai2@users.noreply.github.com>
2018-04-14 22:02:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,177 +1,67 @@
- #cachematrix.R ---> by Neerja  
+ # makeCacheMatrix: this function makes a special "matrix", composed in a list of functions that ultimatey gets the inverse 
+
+# of its matrix
 
 
- 
-## Put comments here that give an overall description of what your 
-## functions do 
-## Write a short comment describing this function 
-##This function Function makeCacheMatrix gets a matrix as an input, set the value of the matrix, 
-#get the value of the matrix, set the inverse Matrix and get the inverse Matrix. The matrix object 
-#can cache its own object.  
-#<<- operator is used to assign a value to an object in an environment that is different  
-#from the current environment  
-#take the matrix as an input 
 
-makeCacheMatrix <- function(x = matrix()) { 
-invMatrix <- NULL    
-#set the value of the Matrix 
-setMatrix <- function(y) { 
-x <<- y 
+makeCacheMatrix <- function(x = matrix()) {    # The function takes the argument x, which is an empty matrix
 
-invMatrix <<- NULL 
+  I <- NULL                                    # I, the inverse of the matrix, is originally set to be an empty object
 
- } 
+  set <- function(y)  {                        # The function 'set' defines the argument, x, as a new empty matrix, y,
 
-getMatrix <- function() x                              #get the value of the Matrix 
+    x <<- y                                    # and redefines I as an empty object
 
-setInverse <- function(inverse) invMatrix <<- inverse  #set the value of the invertible matrix 
-getInverse <- function() invMatrix                     #get the value of the invertible matrix 
+    I <<- NULL
 
-list(setMatrix = setMatrix, getMatrix = getMatrix, 
-      setInverse = setInverse, getInverse = getInverse) 
-} 
+  }
 
-## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an  
-# input and checks inverse matrix from makeCacheMatrix(matrix) has any value in it or not. 
-# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data from  
-# and set the invertible  matrix by using the solve function. 
-# In case inverse matrix from makeCacheMatrix((matrix) has some value in it (always works 
-#after running the code 1st time), it returns a message  "Getting Cached Invertible Matrix"  
-#and the cached object 
+  get <- function() x                          # The function 'get' returns the matrix, x
 
-## Write a short comment describing this function 
- cacheSolve <- function(x, ...) { 
+  
 
-#get the value of the invertible matrix from the makeCacheMatrix function 
-          invMatrix <- x$getInverse() 
+  setInverse <- function(solve)  I <<- solve   # The function 'setInverse' utilizes the 'solve' function in order to 
+
+                                               # calculate the inverse
+
+  getInverse <- function() I                   # The function 'getInverse' returns the inverse of the matrix, I
+
+  
+
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)   # Lists the functions to be carried out
+
+}
 
 
- 
-                if(!is.null(invMatrix)) {                       #if inverse matrix is not NULL 
 
 
- 
- message("Getting Cached Invertible Matrix")   #Type message: Getting Cached Invertible Matrix  
 
-          return(invMatrix)                             #return the invertible matrix 
+# cacheSolve: this funcction calculates the inverse of the special "matrix" created by makeCacheMatrix.
 
+# This is carried out by first checking if the inverse has already been calculated. If so, the inverse matrix, I, is returned.
 
-        } 
-           
-#if value of the invertible matrix is NULL then   
-
-  MatrixData <- x$getMatrix()                     #get the original Matrix Data  
- invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix 
-        x$setInverse(invMatrix)                         #set the invertible matrix  
-   return(invMatrix)                               #return the invertible matrix 
+# If not, then the function calculates the inverse using the setInverse-function, and then the inverse, I, is returned.
 
 
- 
-        ## Return a matrix that is the inverse of 'x' 
 
- 
- } 
+cacheSolve <- function(x, ...) {               # The function takes the argument x, and passes other potential arguments to the ...
 
-#########Testing########## 
+  I <- x$getInverse()                          # Assignes the inverse of the matrix to I, if it already exists
 
- 
-####Test 1 [2*2 Matrix] ##### 
+  if(!is.null(I))  {                           # if-loop which checks if I contains an object
 
-TestMatrix <- matrix(1:4,2,2) 
- 
-TestMatrix 
+    message("getting cached data")             # If the statement checks out, a message is printed and the inverse matrix, I, is returned
 
-CacheMatrix <- makeCacheMatrix(TestMatrix) 
- 
-CacheMatrix$getMatrix() 
- 
-CacheMatrix$getInverse() 
+    return(I)                                  # If not, the computation is carried out, as displayed below
 
-cacheSolve(CacheMatrix) 
+  }
 
+  data <- x$get()                              # The matrix, x, is assigned to the object 'data'
 
- 
-cacheSolve(CacheMatrix) 
+  I <- solve(data, ...)                        # The solve-fuction is assigned to I
 
-####Test 2 [2*2 Matrix] ##### 
+  x$setInverse(I)                              # The solve-function carries out the computation of the inverse
 
-TestMatrix <- matrix(c(1,5,8,2),2,2) 
-TestMatrix 
- 
-CacheMatrix <- makeCacheMatrix(TestMatrix) 
+  I                                            # Return a matrix that is the inverse of 'x'
 
-CacheMatrix$getMatrix() 
-
-CacheMatrix$getInverse() 
-
-cacheSolve(CacheMatrix) 
- 
-cacheSolve(CacheMatrix) 
- 
-####Test 3 [3*3 Matrix] --> singulat Matrix ##### 
-
-#matrix(1:9,3,3) is not possible (singule matrix) because it is giving det(A)  = 0 
-
-#matrix = 1/det(A)[3,-6,3,-6,12,-6,3,-6,3] |det(A) = 1/(1*3 +4* (-6) + 7 *3) = 1/0 
-
-TestMatrix <- matrix(1:9,3,3) 
- 
-TestMatrix 
-
-CacheMatrix <- makeCacheMatrix(TestMatrix) 
- 
-CacheMatrix$getMatrix() 
- 
-CacheMatrix$getInverse() 
-
-
-cacheSolve(CacheMatrix) 
-cacheSolve(CacheMatrix) 
- 
-####Test 4 [3*3 Matrix]##### 
-
-TestMatrix <- matrix(1:8,3,3) 
-
-TestMatrix 
-
-CacheMatrix <- makeCacheMatrix(TestMatrix) 
- 
-CacheMatrix$getMatrix() 
- 
-CacheMatrix$getInverse() 
- 
-cacheSolve(CacheMatrix) 
-
-cacheSolve(CacheMatrix) 
- 
-####Test 5 [4*4 Matrix]##### 
- 
-TestMatrix <- matrix(c(2,3,5,1,3,7,4,5,6,8,0,0,4,5,6,0),4,4) 
- 
-TestMatrix 
- 
-CacheMatrix <- makeCacheMatrix(TestMatrix) 
- 
-CacheMatrix$getMatrix() 
- 
-CacheMatrix$getInverse() 
- 
-cacheSolve(CacheMatrix) 
- 
-cacheSolve(CacheMatrix) 
- 
-####Test 6 [4*4 Matrix]  --> singular Matrix ##### 
- 
-TestMatrix <- matrix(5:21,4,4) 
- 
-TestMatrix 
- 
-CacheMatrix <- makeCacheMatrix(TestMatrix) 
-
-CacheMatrix$getMatrix() 
- 
-CacheMatrix$getInverse() 
- 
-cacheSolve(CacheMatrix) 
-
-cacheSolve(CacheMatrix)
+}
